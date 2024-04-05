58fdfabb053f8cea1c8ceb740c589b39a67fcf9d
CristianVelez <cristiansep0293@gmail.com>
2024-04-05 01:50:14 -0500

    Generacion del build del proyecto y publicacion en el repositorio

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://cristiansep0293.github.io/weather-react",
   server: {
     port: 5500,
     https: false,
